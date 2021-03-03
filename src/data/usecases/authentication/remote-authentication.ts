ad9799b24bb64c8f9286cca61d3793a186b942f5
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-02 22:56:29 -0300

    feat: ensure RemoteAuthentication throws Unexpected on default cases

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,7 +1,7 @@
 import { HttpPostClient } from "@/data/protocols/http/http-post-client";
-import { HttpPostClient } from "@/data/protocols/http/http-post-client";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
-import { InvalidCredentialsError } from "@/domain/errors/invalid-credrntials-error";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
+import { UnexpectedError } from "@/domain/errors/unexpected-error";
 import { AuthenticationParams } from "@/domain/usecases/authentication";
 
 export class RemoteAuthentication {
@@ -16,9 +16,10 @@ export class RemoteAuthentication {
       body: params,
     })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break;
       // case HttpStatusCode.ok: return httpResponse.body;
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: return Promise.resolve()
+      default: throw new UnexpectedError()      
     }
   }
 }
