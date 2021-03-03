4ed03edd775e40d5ff5614965898342f32e3029d
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-02 23:32:46 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,11 @@
-import { HttpPostClientSpy } from "@/data/test/mock-http-client";
-import { RemoteAuthentication } from "./remote-authentication";
-import faker from 'faker'
-import { mockAuthentication, mockAuthenticationModel } from "@/domain/test/mock-account";
-import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
-import { HttpStatusCode } from "@/data/protocols/http/http-response";
-import { UnexpectedError } from "@/domain/errors/unexpected-error";
-import { AccountModel } from "@/domain/models/account-model";
-import { AuthenticationParams } from "@/domain/usecases/authentication";
+  import { HttpPostClientSpy } from "@/data/test";
+  import { RemoteAuthentication } from "./remote-authentication";
+  import faker from 'faker'
+  import { mockAuthentication, mockAuthenticationModel } from "@/domain/test";
+  import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors";
+  import { HttpStatusCode } from "@/data/protocols/http";  
+  import { AccountModel } from "@/domain/models/account-model";
+  import { AuthenticationParams } from "@/domain/usecases/authentication";
 
 type SutTypes = {
   sut: RemoteAuthentication;
