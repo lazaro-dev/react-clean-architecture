035c8c579b0df9be9a7268a20ae49936ee4e8c89
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-04 11:24:05 -0300

    chore: add react

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -10,14 +10,14 @@ export class RemoteAuthentication implements Authentication {
     ){}
 
   async auth(params: AuthenticationParams): Promise<AccountModel>{
-    const httpResponse= await this.httpPostClient.post({
+    const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params,
     })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: return httpResponse.body;
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: throw new UnexpectedError()      
+      default: throw new UnexpectedError()
     }
   }
 }
