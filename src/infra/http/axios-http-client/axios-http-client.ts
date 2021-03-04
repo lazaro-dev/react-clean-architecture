44bccee90513fa05aac57b82db27db4814b948d9
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-03 21:11:08 -0300

    feat: ensure AxiosHttpCLient calls axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,26 @@
+// import { HttpRequest, HttpResponse, HttpClient, HttpPostParams } from '@/data/protocols/http'
+import { HttpPostParams } from '@/data/protocols/http'
+
+import axios, { AxiosResponse } from 'axios'
+
+export class AxiosHttpClient {
+  async post (params: HttpPostParams<any>): Promise<void> {
+
+    await axios(params.url);
+    // let axiosResponse: AxiosResponse
+    // try {
+    //   axiosResponse = await axios.request({
+    //     url: data.url,
+    //     method: data.method,
+    //     data: data.body,
+    //     headers: data.headers
+    //   })
+    // } catch (error) {
+    //   axiosResponse = error.response
+    // }
+    // return {
+    //   statusCode: axiosResponse.status,
+    //   body: axiosResponse.data
+    // }
+  }
+}
