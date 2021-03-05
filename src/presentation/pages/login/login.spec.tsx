d2b346a8e3fdc50e4af296c3ecd092c812d71bc2
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-05 16:16:59 -0300

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,6 +9,12 @@ describe('Login Component', () => {
     expect(errorWrap.childElementCount).toBe(0);
     const submitButton = getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
+    const emailStatus = getByTestId('email-status');
+    expect(emailStatus.title).toBe('Campo obrigatório');
+    expect(emailStatus.textContent).toBe('N');
+    const passwordStatus = getByTestId('password-status');
+    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.textContent).toBe('N');
   })
 })
 
