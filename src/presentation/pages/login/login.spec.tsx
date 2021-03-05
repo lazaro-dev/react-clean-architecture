a7d555e86ac41126be2ec869b36a072d0203576b
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-05 17:05:31 -0300

    test: ensure login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -53,5 +53,14 @@ describe('Login Component', () => {
     })
   });
 
+  test('should call validation with correct password', () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId('password');
+    fireEvent.input(passwordInput, {target: { value: 'any_password'}});
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  });
+
 })
 
