4901752b46522bb2417a015020963631d6200cdf
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-05 19:39:59 -0300

    feat: ensure login disable submit button only if form is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -3,8 +3,7 @@ import { Validation } from "@/presentation/protocols/validation";
 export class ValidationStub implements Validation {
   errorMessage: string;
 
-  validate(fildName: string, fildValue: string): string {
-
+  validate (fildName: string, fildValue: string): string {
     return this.errorMessage;
   }
 }
