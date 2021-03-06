72aa55ccc70f915f690258684167f13db8808407
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-06 12:37:43 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -3,7 +3,8 @@ import { FieldValidation } from "@/validation/protocols/field-validation";
 
 export class EmailValidation implements FieldValidation {
   constructor (readonly field: string){}
-  validate(field: string): Error {
-    return new InvalidFieldError();
+  validate(value: string): Error {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
