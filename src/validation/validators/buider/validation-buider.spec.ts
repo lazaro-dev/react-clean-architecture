cc720bdcd213ee9ed652ad4a4142a0f0e83e293d
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-06 15:17:27 -0300

    test: ensure ValidationBuider has minLength helper

--- src/validation/validators/buider/validation-buider.spec.ts
+++ src/validation/validators/buider/validation-buider.spec.ts
@@ -1,14 +1,19 @@
-import { RequiredFieldValidation, EmailValidation } from "@/validation/validators";
+import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from "@/validation/validators";
 import { ValidationBuilder as sut } from "./validation-buider";
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
     const validations = sut.field('any_field').required().build();
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
-  })
+  });
 
   test('should return EmailValidation', () => {
     const validations = sut.field('any_field').email().build();
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
-  })
+    expect(validations).toEqual([new EmailValidation('any_field')])
+  });
+
+  test('should return MinLengthValidation', () => {
+    const validations = sut.field('any_field').min(5).build();
+    expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+  });
 })
