332921fb01137b4acb0bae5ec29c74057f1206a3
Lázaro José Marques Sobral <lazaro.sobral@facape.br>
2021-03-05 22:59:15 -0300

    test: ensure login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -17,7 +17,7 @@ type SutParams = {
   validationError: string
 }
 
-const history = createMemoryHistory();
+const history = createMemoryHistory({ initialEntries: ['/login']});
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
   const authenticationSpy = new AuthenticationSpy();
@@ -165,6 +165,8 @@ describe('Login Component', () => {
     simulateValidSubmit(sut);
     await waitFor(() => sut.getByTestId('form'));
     expect(localStorage.setItem).toHaveBeenLastCalledWith('accessToken', authenticationSpy.account.accessToken);
+    expect(history.length).toBe(1);
+    expect(history.location.pathname).toBe('/');
   });
 
   test('should add go to signup page', () => {
